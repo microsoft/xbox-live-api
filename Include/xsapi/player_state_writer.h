97cb7d655850c18b71b168f74203719e8b321ccb
Blake Gross <blgross@microsoft.com>
2016-12-07 12:17:21 -0800

    stuff

---
+++ Include/xsapi/player_state_writer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace xbox { namespace services { namespace player_state_writer {
+
+class player_state_writer
+{
+    static player_state_writer get_singleton_instance();
+
+};
+    
+}}}
