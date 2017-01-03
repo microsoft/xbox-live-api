c2f4882ff81ccac2d77a22a987af171e1a4aa947
Jason Sandlin <jasonsa@microsoft.com>
2017-01-03 12:47:22 -0800

    Fixing date parsing when day is single digit

--- Utilities/VSOBuildScripts/preCompileScript.cmd
+++ Utilities/VSOBuildScripts/preCompileScript.cmd
@@ -4,12 +4,18 @@ echo Running preCompileScript.cmd
 
 call %TFS_SourcesDirectory%\setBuildVersion.cmd
 
+for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G
+
+set DATETIME_YEAR=%datetime:~0,4%
+set DATETIME_MONTH=%datetime:~4,2%
+set DATETIME_DAY=%datetime:~6,2%
+
 rem format release numbers
 FOR /F "TOKENS=1 eol=/ DELIMS=. " %%A IN ("%TFS_VersionNumber%") DO SET SDK_POINT_NAME_YEARMONTH=%%A
 FOR /F "TOKENS=2 eol=/ DELIMS=. " %%A IN ("%TFS_VersionNumber%") DO SET SDK_POINT_NAME_DAYVER=%%A
-set SDK_POINT_NAME_YEAR=%SDK_POINT_NAME_YEARMONTH:~0,2%
-set SDK_POINT_NAME_MONTH=%SDK_POINT_NAME_YEARMONTH:~2,2%
-set SDK_POINT_NAME_DAY=%SDK_POINT_NAME_DAYVER:~0,2%
+set SDK_POINT_NAME_YEAR=%DATETIME_YEAR%
+set SDK_POINT_NAME_MONTH=%DATETIME_MONTH%
+set SDK_POINT_NAME_DAY=%DATETIME_DAY%
 set SDK_POINT_NAME_VER=%SDK_POINT_NAME_DAYVER:~2,9%
 
 set SDK_RELEASE_NAME=%SDK_RELEASE_YEAR:~2,2%%SDK_RELEASE_MONTH%
