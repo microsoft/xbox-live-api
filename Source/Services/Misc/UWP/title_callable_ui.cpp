1b472a801bb72ab5f0e31c46d9a5d3661251a305
Jason Sandlin <jasonsa@microsoft.com>
2017-02-26 12:47:54 -0800

    Fixing WinRT build (#86)

--- Source/Services/Misc/UWP/title_callable_ui.cpp
+++ Source/Services/Misc/UWP/title_callable_ui.cpp
@@ -492,7 +492,8 @@ title_callable_ui::_Get_gaming_privilege_scope_policy(
         localConfig->sandbox(),
         localConfig->environment_prefix(),
         localConfig->environment(),
-        localConfig->use_first_party_token()
+        localConfig->use_first_party_token(),
+        g_is_xbox_live_creators_sdk
         );
 
     scope = ref new Platform::String(authConfig.rps_ticket_service().c_str());
