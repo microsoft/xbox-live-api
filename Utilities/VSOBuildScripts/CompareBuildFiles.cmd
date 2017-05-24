ff4cb6155aa3d1a4d6fd3b272b1a44da573bc904
Jason Sandlin <jasonsa@microsoft.com>
2017-05-24 16:49:25 -0700

    Moving Ship vcxprojs flavors to generate on build server via cmake

---
+++ Utilities/VSOBuildScripts/CompareBuildFiles.cmd
@@ -0,0 +1,66 @@
+if "%1" EQU "" goto help
+set ROOT_FOLDER=%1
+set NEW_FOLDER=%ROOT_FOLDER%\Utilities\CMake\output
+set OLD_FOLDER=%ROOT_FOLDER%\Build
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.110.XDK.Cpp\Microsoft.Xbox.Services.110.XDK.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.110.XDK.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.110.XDK.Cpp\Microsoft.Xbox.Services.110.XDK.Ship.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.110.XDK.Ship.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.110.XDK.WinRT\Microsoft.Xbox.Services.110.XDK.WinRT.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.110.XDK.WinRT.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.140.UWP.Cpp\Microsoft.Xbox.Services.140.UWP.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.140.UWP.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.140.UWP.Cpp\Microsoft.Xbox.Services.140.UWP.Ship.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.140.UWP.Ship.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.140.UWP.WinRT\Microsoft.Xbox.Services.140.UWP.WinRT.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.140.UWP.WinRT.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.140.XDK.Cpp\Microsoft.Xbox.Services.140.XDK.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.140.XDK.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.140.XDK.Cpp\Microsoft.Xbox.Services.140.XDK.Ship.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.140.XDK.Ship.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.141.UWP.Cpp\Microsoft.Xbox.Services.141.UWP.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.141.UWP.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.141.UWP.Cpp\Microsoft.Xbox.Services.141.UWP.Ship.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.141.UWP.Ship.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.141.XDK.Cpp\Microsoft.Xbox.Services.141.XDK.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.141.XDK.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.141.XDK.Cpp\Microsoft.Xbox.Services.141.XDK.Ship.Cpp.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.141.XDK.Ship.Cpp.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.UnitTest.140.TAEF\Microsoft.Xbox.Services.UnitTest.140.TAEF.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.UnitTest.140.TAEF.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+fc %OLD_FOLDER%\Microsoft.Xbox.Services.UnitTest.140.TE\Microsoft.Xbox.Services.UnitTest.140.TE.vcxproj %NEW_FOLDER%\Microsoft.Xbox.Services.UnitTest.140.TE.vcxproj
+if %ERRORLEVEL% NEQ 0 goto email
+
+goto done
+
+:email
+if "%2" NEQ "emailfailures" goto done
+set MSGTITLE="REGEN BUILD FILES: %BUILD_SOURCEVERSIONAUTHOR% %BUILD_DEFINITIONNAME% %BUILD_SOURCEBRANCH% = %agent.jobstatus%"
+set MSGBODY="%TFS_DROPLOCATION%    https://microsoft.visualstudio.com/OS/_build/index?buildId=%BUILD_BUILDID%&_a=summary"
+call \\scratch2\scratch\jasonsa\tools\send-build-email.cmd %MSGTITLE% %MSGBODY% 
+
+goto done
+:help
+@echo off
+echo.
+echo CompareBuildFiles.cmd rootFolder [emailfailures]
+echo.
+echo Example:
+echo CompareBuildFiles.cmd C:\git\forks\xbox-live-api
+echo.
+
+:done
