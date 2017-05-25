f01171fd4dfff82850b1823dd4f6cf9fdafc72da
Jason Sandlin <jasonsa@microsoft.com>
2017-05-24 18:39:17 -0700

    Fixing forward decl (#168)

--- Include/xsapi/presence.h
+++ Include/xsapi/presence.h
@@ -5,9 +5,9 @@
 #include "xsapi/real_time_activity.h"
 
 #if !XSAPI_U
-namespace xbox { namespace services { namespace experimental { namespace social { namespace manager {
+namespace xbox { namespace services { namespace social { namespace manager {
     enum class change_list_enum;
-} } } } }
+} } } }
 #endif
 
 namespace xbox { namespace services {
