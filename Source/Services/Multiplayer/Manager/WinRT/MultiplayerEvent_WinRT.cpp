93c435d5ea69bc3e8a93699286bd5213290864a6
Jason Sandlin <tripleastudios.admin@gmail.com>
2017-05-25 23:49:05 -0700

    Removing pragma once from cpp files

--- Source/Services/Multiplayer/Manager/WinRT/MultiplayerEvent_WinRT.cpp
+++ Source/Services/Multiplayer/Manager/WinRT/MultiplayerEvent_WinRT.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#pragma once
+
 #include "pch.h"
 #include "MultiplayerEvent_WinRT.h"
 #include "UserAddedEventArgs_WinRT.h"
