831f9daf27168b14e0afc0ced343d94dfa2ec2fa
Jason Sandlin <jasonsa@microsoft.com>
2017-08-28 16:34:07 -0700

    Adding tests for old Win10 OS SKUs

---
+++ Tests/OldOS/10586.ByNuGet/MainPage.xaml.h
@@ -0,0 +1,28 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+#include "xsapi\services.h"
+
+namespace App1
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void buttonSignInSilent_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void buttonSignIn_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+		xbox::services::system::xbox_live_user m_user;
+
+	};
+}
