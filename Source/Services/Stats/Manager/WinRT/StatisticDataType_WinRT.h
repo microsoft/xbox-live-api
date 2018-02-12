eac1b77a35f1edfae8e7e825d0f5b39155d37e0e
Kevin Asgari <kevinasg@microsoft.com>
2018-02-12 14:39:39 -0800

    Fix up WinRT code comments for stats manager APIs

--- Source/Services/Stats/Manager/WinRT/StatisticDataType_WinRT.h
+++ Source/Services/Stats/Manager/WinRT/StatisticDataType_WinRT.h
@@ -6,10 +6,24 @@
 
 NAMESPACE_MICROSOFT_XBOX_SERVICES_STATISTIC_MANAGER_BEGIN
 
+/// <summary> 
+/// Indicates the type of data of the statistic.
+/// </summary>
 public enum class StatisticDataType
 {
+	/// <summary> 
+	/// The stat data type is undefined.
+	/// </summary>
     Undefined = xbox::services::stats::manager::stat_data_type::undefined,
+
+	/// <summary> 
+	/// The stat data type is a number.
+	/// </summary>
     Number = xbox::services::stats::manager::stat_data_type::number,
+
+	/// <summary> 
+	/// The stat data type is a string.
+	/// </summary>
     String = xbox::services::stats::manager::stat_data_type::string
 };
 
