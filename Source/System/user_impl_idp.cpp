9367b03b7e6a960154e55e1cddd0b7bbcf5fc125
John L <johlafo@microsoft.com>
2018-05-29 12:00:11 -0700

    Fixing build break (#440)

--- Source/System/user_impl_idp.cpp
+++ Source/System/user_impl_idp.cpp
@@ -275,6 +275,7 @@ void user_impl_idp::initialize_provider(
                 }
 
                 AsyncBlock* async = data->async;
+                std::weak_ptr<user_impl_idp> thisWeakPtr(thisPtr);
                 asyncOp->Completed = ref new AsyncOperationCompletedHandler<WebAccountProvider^>(
                     [thisWeakPtr, async](IAsyncOperation<WebAccountProvider^>^ asyncInfo, Windows::Foundation::AsyncStatus status)
                 {
