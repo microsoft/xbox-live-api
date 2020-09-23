1e08126dc03bb385ff1e314e8292092e7cccdea4
Ramsey <rakhad@microsoft.com>
2020-09-23 12:38:04 -0700

    Remove the virtual dtor from social_manager (#537)

--- Include/xsapi/social_manager.h
+++ Include/xsapi/social_manager.h
@@ -769,8 +769,6 @@ private:
 class social_manager : public std::enable_shared_from_this<social_manager>
 {
 public:
-    virtual ~social_manager() {}
-
     /// <summary>
     /// Gets the social_manager singleton instance
     /// </summary>
